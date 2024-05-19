903203636cdb8f6e9d5ec49220b61dc341bafec3
SAMUEL SATURNO <120234429+Samuel-Oliveira-saturno@users.noreply.github.com>
2024-05-18 23:30:05 -0300

    Add files via upload

---
+++ sketch_may15a.ino
@@ -0,0 +1,28 @@
+/************************************************
+ * RoboCore - Kit Robo Explorer - Blink
+ *
+ * Codigo que faz o LED interno da Vespa
+ * piscar com intervalos de 1 segundo
+ ***********************************************/
+
+//Declaracao da variavel do pino do LED
+const int LED_Vespa = 15;
+
+void setup() {
+
+  //Declaracao do pino como saida
+  pinMode(LED_Vespa, OUTPUT);
+  
+}
+
+void loop() {
+
+  //Acende o LED durante 1 segundo
+  digitalWrite(LED_Vespa, HIGH);
+  delay(1000);
+
+  //Apaga o LED durante 1 segundo
+  digitalWrite(LED_Vespa, LOW);
+  delay(1000);
+  
+}
